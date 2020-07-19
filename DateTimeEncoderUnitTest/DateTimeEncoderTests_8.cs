f4d0ba1a400c96f8cb10a38ac4c80ccf183f8b81
Moin Abbas Qureshi <qureshi@stud.fra-uas.de>
2020-07-19 16:18:00 +0200

    commit

---
+++ DateTimeEncoderUnitTest/DateTimeEncoderTests_8.cs
@@ -0,0 +1,272 @@
+﻿using DateTimeEncoderLib;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NeoCortexApi.Network;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+
+namespace DateTimeEncoderUnitTest
+{
+    /// <summary>
+    /// Performing the unit tests for the datetime encoder, date encoder only and time encoder only by using class of DateTimeEncoderTests_1.
+    /// </summary>
+    [TestClass]
+
+    public class DateTimeEncoderTests_8
+    {
+        /// <summary>
+        /// Performing unit test when width = 4 and radius = 4, to verify the datetime encoder. 
+        /// </summary>
+        /// <param name="input"></param>
+        /// <param name="expectedOutput"></param>
+        [TestMethod]
+
+        //When width (W) = 4 and radius = 4:
+        [DataRow("04/29/2020 09:14:31", new int[] { 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 })]
+        [DataRow("01/01/2010 00:00:00", new int[] { 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 })]
+        [DataRow("09/13/2019 00:19:01", new int[] { 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 })]
+        [DataRow("12/15/2023 16:10:56", new int[] { 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1 })]
+        [DataRow("10/29/2026 06:10:27", new int[] { 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 })]
+        [DataRow("03/16/2004 12:53:57", new int[] { 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1 })]
+
+        public void EncodeTest1(Object input, int[] expectedOutput)
+        {
+            CortexNetworkContext ctx = new CortexNetworkContext();
+
+            Dictionary<string, object> encoderSettings = getDefaultSettings();
+
+            DateTimeEncoder encoder = new DateTimeEncoder(encoderSettings);
+
+            var result = encoder.Encode(input);
+
+            foreach (var item in result)
+            {
+                Debug.Write(item + ",");
+            }
+            Assert.IsTrue(result.SequenceEqual(expectedOutput));
+        }
+
+        /// <summary>
+        /// Performing unit test when width = 4 and radius = 4, to verify the date encoder only.
+        /// </summary>
+        /// <param name="input"></param>
+        /// <param name="expectedOutput"></param>
+
+        [TestMethod]
+
+        //When width (W) = 4 and radius = 4:
+        [DataRow("04/29/2020", new int[] { 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0 })]
+        [DataRow("01/01/2020", new int[] { 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0 })]
+        [DataRow("09/13/2019", new int[] { 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1 })]
+        [DataRow("04/29/2020", new int[] { 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0 })]
+        [DataRow("10/29/2026", new int[] { 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1 })]
+        [DataRow("03/16/2004", new int[] { 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0 })]
+        [DataRow("07/04/2016", new int[] { 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1 })]
+
+        public void EncodeDateOnlyTest(Object input, int[] expectedOutput)
+        {
+            CortexNetworkContext ctx = new CortexNetworkContext();
+
+            Dictionary<string, object> encoderSettings = getDefaultSettings();
+
+            DateTimeEncoder encoder = new DateTimeEncoder(encoderSettings);
+
+            var result = encoder.EncodeDateOnly(input);
+
+            foreach (var item in result)
+            {
+                Debug.Write(item + ",");
+            }
+            Assert.IsTrue(result.SequenceEqual(expectedOutput));
+        }
+
+        /// <summary>
+        /// Performing unit test when width = 4 and radius = 4, to verify the time encoder only.
+        /// </summary>
+        /// <param name="input"></param>
+        /// <param name="expectedOutput"></param>
+
+        [TestMethod]
+
+        //When width (W) = 4 and radius = 4:
+        [DataRow("09:14:31", new int[] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 })]
+        [DataRow("00:00:00", new int[] { 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 })]
+        [DataRow("00:19:01", new int[] { 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 })]
+        [DataRow("16:10:56", new int[] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1 })]
+        [DataRow("06:10:27", new int[] { 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 })]
+        [DataRow("12:53:57", new int[] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1 })]
+
+        public void EncodeTimeOnlyTest(Object input, int[] expectedOutput)
+        {
+            CortexNetworkContext ctx = new CortexNetworkContext();
+
+            Dictionary<string, object> encoderSettings = getDefaultSettings();
+
+            DateTimeEncoder encoder = new DateTimeEncoder(encoderSettings);
+
+            var result = encoder.EncodeTimeOnly(input);
+
+            foreach (var item in result)
+            {
+                Debug.Write(item + ",");
+            }
+            Assert.IsTrue(result.SequenceEqual(expectedOutput));
+        }
+
+        /// <summary>
+        /// For the decoder implementation of the encoder.  
+        /// </summary>
+        public void DecodeTest()
+        {
+            // TODO.
+        }
+
+        /// <summary>
+        /// Demonstratses how to create an encoder by explicite invoke of initialization.
+        /// </summary>
+        [TestMethod]
+
+        public void InitTest1()
+        {
+            Dictionary<string, object> encoderSettings = getDefaultSettings();
+
+            // We change here value of Name property.
+            encoderSettings["Name"] = "hello";
+
+            // We add here new property.
+            encoderSettings.Add("TestProp1", "hello");
+
+            var encoder = new DateTimeEncoder();
+
+            // Settings can also be passed by invoking Initialize(sett)
+            encoder.Initialize(encoderSettings);
+
+            // Property can also be set this way.
+            encoder["abc"] = "1";
+
+            Assert.IsTrue((string)encoder["TestProp1"] == "hello");
+
+            Assert.IsTrue((string)encoder["Name"] == "hello");
+
+            Assert.IsTrue((string)encoder["abc"] == "1");
+        }
+
+        /// <summary>
+        /// Initializes encoder and sets mandatory properties.
+        /// </summary>
+        [TestMethod]
+        public void InitTest2()
+        {
+            CortexNetworkContext ctx = new CortexNetworkContext();
+
+            Dictionary<string, object> encoderSettings = getDefaultSettings();
+
+            var encoder = ctx.CreateEncoder(typeof(DateTimeEncoder).FullName, encoderSettings);
+
+            foreach (var item in encoderSettings)
+            {
+                Assert.IsTrue(item.Value == encoder[item.Key]);
+            }
+        }
+
+        /// <summary>
+        /// Demonstratses how to create an encoder and how to set encoder properties by using of context.
+        /// </summary>
+        [TestMethod]
+        public void InitTest3()
+        {
+            CortexNetworkContext ctx = new CortexNetworkContext();
+
+            // Gets set of default properties, which more or less every encoder requires.
+            Dictionary<string, object> encoderSettings = getDefaultSettings();
+
+            // We change here value of Name property.
+            encoderSettings["Name"] = "hello";
+
+            // We add here new property.
+            encoderSettings.Add("TestProp1", "hello");
+
+            var encoder = ctx.CreateEncoder(typeof(DateTimeEncoder).FullName, encoderSettings);
+
+            // Property can also be set this way .
+            encoder["abc"] = "1";
+
+            Assert.IsTrue((string)encoder["TestProp1"] == "hello");
+
+            Assert.IsTrue((string)encoder["Name"] == "hello");
+
+            Assert.IsTrue((string)encoder["abc"] == "1");
+        }
+
+        /// <summary>
+        /// Demonstratses how to create an encoder by explicite invoke of initialization.
+        /// </summary>
+        [TestMethod]
+        public void InitTest4()
+        {
+            Dictionary<string, object> encoderSettings = getDefaultSettings();
+
+            // We change here value of Name property.
+            encoderSettings["Name"] = "hello";
+
+            // We add here new property.
+            encoderSettings.Add("TestProp1", "hello");
+
+            var encoder = new DateTimeEncoder();
+
+            // Settings can also be passed by invoking Initialize(sett)
+            encoder.Initialize(encoderSettings);
+
+            // Property can also be set this way.
+            encoder["abc"] = "1";
+
+            Assert.IsTrue((string)encoder["TestProp1"] == "hello");
+
+            Assert.IsTrue((string)encoder["Name"] == "hello");
+
+            Assert.IsTrue((string)encoder["abc"] == "1");
+        }
+
+        /// <summary>
+        /// Initializes all encoders.
+        /// </summary>
+        [TestMethod]
+        public void InitializeAllEncodersTest()
+        {
+            CortexNetworkContext ctx = new CortexNetworkContext();
+
+            Assert.IsTrue(ctx.Encoders != null && ctx.Encoders.Count > 0);
+
+            Dictionary<string, object> encoderSettings = getDefaultSettings();
+
+            foreach (var item in ctx.Encoders)
+            {
+                var encoder = ctx.CreateEncoder(typeof(DateTimeEncoder).FullName, encoderSettings);
+
+                foreach (var sett in encoderSettings)
+                {
+                    Assert.IsTrue(sett.Value == encoder[sett.Key]);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Values for the radius and width will be passed here. For this unit test the width = 4 and radius = 4.
+        /// </summary>
+        /// <returns></returns>
+        private static Dictionary<string, object> getDefaultSettings()
+        {
+            Dictionary<String, Object> encoderSettings = new Dictionary<string, object>();
+            //encoderSettings.Add("N", 0);
+            encoderSettings.Add("W", 4);
+            //encoderSettings.Add("MinVal", (double)0);
+            //encoderSettings.Add("Max2Val", (double)0);
+            encoderSettings.Add("Radius", (double)4);
+            //encoderSettings.Add("Resolution", (double)0);
+            //encoderSettings.Add("Periodic", (bool)false);
+            //encoderSettings.Add("ClipInput", (bool)true); 
+            return encoderSettings;
+        }
+    }
+}
